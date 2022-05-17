d487e4c0240389fcfaa8d51a020019d4ff84a31f
Taras Machyshyn <taras@espocrm.com>
2022-05-17 16:07:49 +0300

    2022 year

--- src/files/application/Espo/Modules/ExportImport/Tools/Import/Placeholder/Actions/Config/ObjectData.php
+++ src/files/application/Espo/Modules/ExportImport/Tools/Import/Placeholder/Actions/Config/ObjectData.php
@@ -1,17 +1,17 @@
 <?php
 /************************************************************************
- * This file is part of Demo Data extension for EspoCRM.
+ * This file is part of Export Import extension for EspoCRM.
  *
- * Demo Data extension for EspoCRM.
- * Copyright (C) 2014-2021 Yurii Kuznietsov, Taras Machyshyn, Oleksii Avramenko
+ * Export Import extension for EspoCRM.
+ * Copyright (C) 2014-2022 Yurii Kuznietsov, Taras Machyshyn, Oleksii Avramenko
  * Website: https://www.espocrm.com
  *
- * Demo Data extension is free software: you can redistribute it and/or modify
+ * Export Import extension is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Demo Data extension is distributed in the hope that it will be useful,
+ * Export Import extension is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
