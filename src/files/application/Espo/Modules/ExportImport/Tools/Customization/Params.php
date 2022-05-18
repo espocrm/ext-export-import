75205967b1508fb171c8d7518d6baa0ea255ac39
Taras Machyshyn <taras@espocrm.com>
2022-05-18 16:16:50 +0300

    Added Customization copy

---
+++ src/files/application/Espo/Modules/ExportImport/Tools/Customization/Params.php
@@ -0,0 +1,141 @@
+<?php
+/************************************************************************
+ * This file is part of Export Import extension for EspoCRM.
+ *
+ * Export Import extension for EspoCRM.
+ * Copyright (C) 2014-2022 Yurii Kuznietsov, Taras Machyshyn, Oleksii Avramenko
+ * Website: https://www.espocrm.com
+ *
+ * Export Import extension is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Export Import extension is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with EspoCRM. If not, see http://www.gnu.org/licenses/.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU General Public License version 3.
+ ************************************************************************/
+
+namespace Espo\Modules\ExportImport\Tools\Customization;
+
+use Espo\Core\{
+    Utils\Util,
+};
+
+use Espo\Modules\ExportImport\Tools\{
+    Manifest,
+    Params as ToolParams,
+};
+
+class Params
+{
+    public const PATH_CUSTOM = 'custom/Espo/Custom';
+
+    private $path;
+
+    private $exportImportDefs;
+
+    private $manifest;
+
+    private $entityTypeList;
+
+    public static function create(): self
+    {
+        return new self();
+    }
+
+    public function withPath(string $path): self
+    {
+        $obj = clone $this;
+
+        $obj->srcPath = $path;
+
+        return $obj;
+    }
+
+    public function withExportImportDefs(array $exportImportDefs): self
+    {
+        $obj = clone $this;
+
+        $obj->exportImportDefs = $exportImportDefs;
+
+        return $obj;
+    }
+
+    public function withManifest(Manifest $manifest): self
+    {
+        $obj = clone $this;
+
+        $obj->manifest = $manifest;
+
+        return $obj;
+    }
+
+    public function withEntityTypeList(array $entityTypeList): self
+    {
+        $obj = clone $this;
+
+        $obj->entityTypeList = $entityTypeList;
+
+        return $obj;
+    }
+
+    /**
+     * Get path
+     */
+    public function getPath(): string
+    {
+        return $this->path;
+    }
+
+    /**
+     * Get path
+     */
+    public function getCustomizationPath(): string
+    {
+        return Util::concatPath(
+            $this->path,
+            ToolParams::PATH_CUSTOMIZATION
+        );
+    }
+
+    /**
+     * Get custom path
+     */
+    public function getSystemCustomPath(): string
+    {
+        return self::PATH_CUSTOM;
+    }
+
+    /**
+     * Get exportImport defs
+     */
+    public function getExportImportDefs(): array
+    {
+        return $this->exportImportDefs;
+    }
+
+    /**
+     * Get a manifest
+     */
+    public function getManifest(): Manifest
+    {
+        return $this->manifest;
+    }
+
+    /**
+     * Get entity type list
+     */
+    public function getEntityTypeList(): string
+    {
+        return $this->entityTypeList;
+    }
+}
---
+++ src/files/application/Espo/Modules/ExportImport/Tools/Customization/Processors/Export.php
@@ -0,0 +1,52 @@
+<?php
+/************************************************************************
+ * This file is part of Export Import extension for EspoCRM.
+ *
+ * Export Import extension for EspoCRM.
+ * Copyright (C) 2014-2022 Yurii Kuznietsov, Taras Machyshyn, Oleksii Avramenko
+ * Website: https://www.espocrm.com
+ *
+ * Export Import extension is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Export Import extension is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with EspoCRM. If not, see http://www.gnu.org/licenses/.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU General Public License version 3.
+ ************************************************************************/
+
+namespace Espo\Modules\ExportImport\Tools\Customization\Processors;
+
+use Espo\Core\{
+    Di,
+};
+
+use Espo\Modules\ExportImport\Tools\{
+    Customization\Processor,
+    Customization\Params,
+};
+
+class Export implements
+    Processor,
+    Di\FileManagerAware
+{
+    use Di\FileManagerSetter;
+
+    public function process(Params $params): void
+    {
+        $src = $params->getSystemCustomPath();
+        $dest = $params->getCustomizationPath();
+
+        //todo: copy only $params->getEntityTypeList()
+        $this->fileManager->copy($src, $dest, true);
+    }
+}
